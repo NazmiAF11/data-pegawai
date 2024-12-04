e93bae7528b3bf560f5832d69313556472ce9601
NazmiAF11 <114555528+NazmiAF11@users.noreply.github.com>
2024-12-04 20:10:53 +0700

    Add files via upload

---
+++ tugas nazmi/tambah.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Tambah Data Pegawai</h1>
+    <form action="proses,php" mehod="POST">
+        <label>NAMA:></label>
+        <input type="text" name="nama" required><br>
+        <label>EMAIL:></label>
+        <input type="text" name="nama" required><br>
+        <label>POSISI:></label>
+        <input type="text" name="nama" required><br>
+        <label>GAJI:></label>
+        <input type="text" name="nama" required><br>
+        <button type="sumbit">SIMPAN</button>  
+    </form>
+    <a href="index.php">KEMBALI</a>
+</body>
+</html>
